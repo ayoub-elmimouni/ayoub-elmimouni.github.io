0bcde02039a4f9e6783a819ab5fcbf7934ac9512
ayoub-elmimouni <ayoub.elmimouni7@gmail.com>
2024-11-05 15:58:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Student", "Marketer", "Designer", "Freelancer"],
+        strings: ["Student", "Futur Marketer", "Futur Designer", "Futur Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Student", "Marketer", "Designer", "Freelancer"],
+        strings: ["Student", "Futur Marketer", "Futur Designer", "Futur Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
